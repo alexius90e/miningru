74a0d170d74b39ee208da6f447d0f2119e33a81b
Aliaksei Karzhou <alexius90e@gmail.com>
2025-03-18 20:58:28 +0300

    feat: added blog-posts

--- js/index.js
+++ js/index.js
@@ -14,3 +14,32 @@ if (headerBurgerButton && headerNav) {
     }
   });
 }
+
+const blogPosts = document.querySelectorAll('.blog-posts__post');
+
+blogPosts.forEach((post) => {
+  const expandEl = post.querySelector('.blog-posts__post-expand');
+  const textEl = post.querySelector('.blog-posts__post-text');
+  const heightLimit = 140;
+
+  if (textEl && expandEl && textEl.scrollHeight > heightLimit) {
+    expandEl.style.display = 'flex';
+    textEl.style.maxHeight = `${heightLimit}px`;
+  }
+
+  post.addEventListener('click', (event) => {
+    const isExpandBtn = event.target.classList.contains('blog-posts__post-expand-button');
+
+    if (textEl && expandEl && isExpandBtn) {
+      if (textEl.style.maxHeight !== `${heightLimit}px`) {
+        textEl.style.maxHeight = `${heightLimit}px`;
+        expandEl.style.marginTop = -expandEl.scrollHeight + 'px';
+        event.target.innerText = 'Развернуть пост'
+      } else {
+        textEl.style.maxHeight = textEl.scrollHeight + 'px';
+        expandEl.style.marginTop = -20 + 'px';
+        event.target.innerText = 'Cвернуть пост'
+      }
+    }
+  });
+});
